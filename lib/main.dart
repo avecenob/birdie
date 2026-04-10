0dbb840b152821d872477fc10cebf94c2c99f3de
Arya Baskara <aryabasc@gmail.com>
2026-04-10 11:10:33 +0700

    feat: convert GamePage to StatefulWidget and manage game state

--- lib/main.dart
+++ lib/main.dart
@@ -51,9 +51,14 @@ class Tile extends StatelessWidget {
   }
 }
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   GamePage({super.key});
 
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -61,19 +66,27 @@ class GamePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
           for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (var letter in guess) Tile(letter.char, letter.type),
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 2.5,
+                      vertical: 2.5,
+                    ),
+                    child: Tile(letter.char, letter.type),
+                  ),
               ],
             ),
           GuessInput(
             onSubmitGuess: (String guess) {
-              print(guess);
-            },
+              setState(() {
+                _game.guess(guess);
+              });
+            }
           ),
         ],
       ),
